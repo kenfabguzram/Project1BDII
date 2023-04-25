c77890f08bd851d9dbad211c182580f81d8d9950
kpalaciosm02 <60985460+kpalaciosm02@users.noreply.github.com>
2023-04-25 03:04:31 -0600

    List shows in app.vue, need to show components in it.

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,11 +1,34 @@
-<script setup lang="ts">
-import Item from './components/Item.vue'
+<script lang="ts">
+//import Item from './components/Item.vue'
+  export default {
+    name: 'itemList',
+    data(){
+      return {
+        items : [
+          { title: 'Archivo 1', data: 'Datos del archivo 1', isFile: true },
+          { title: 'Archivo 2', data: 'Datos del archivo 2', isFile: true },
+          { title: 'Carpeta 1', data: 'Datos de la carpeta 1', isFile: false }
+        ]    
+      }
+    }
+  }
 </script>
 
 <template>
-  <Item title="Nombre del archivo" data="Datos del archivo" :isFile="false"></Item>
+  <!--<Item title="Nombre del archivo" data="Datos del archivo" :isFile="true"></Item>
+  <Item title="Nombre del archivo2" data="Datos del archivo3" :isFile="true"></Item>
+  <Item title="Nombre de la carpeta" data="Datos de la carpeta" :isFile="false"></Item>-->
+  <div v-for="item in items">
+    <p class="title">{{ item.title }}</p>
+    <p class="data"> {{ item.data }}</p>
+  </div>
 </template>
 
 <style scoped>
-  
+  .title{
+    color:black
+  }
+  .data{
+    color:cadetblue
+  }
 </style>
---
+++ vue-app/src/components/itemList.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import Item from './Item.vue';
+
+    export default {
+        name: 'itemList',
+        data(){
+            return {
+                items : [
+                    { title: 'Archivo 1', data: 'Datos del archivo 1', isFile: true },
+                    { title: 'Archivo 2', data: 'Datos del archivo 2', isFile: true },
+                    { title: 'Carpeta 1', data: 'Datos de la carpeta 1', isFile: false }
+                ]    
+            }
+        }
+    }
+</script>
+
+    
+<template>
+    <div v-for="item in items">
+        <p>{{ item.title }}</p>
+        <p> {{ item.data }}</p>
+    </div>
+</template>
+
+<style>
+    /*#app {
+
+    }*/
+</style>
