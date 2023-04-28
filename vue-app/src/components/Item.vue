188eca8afa9a5823fcde438512bd2ec54d9129ea
kpalaciosm02 <60985460+kpalaciosm02@users.noreply.github.com>
2023-04-28 17:14:09 -0600

    File manager frontend almost ready.

--- vue-app/src/components/Item.vue
+++ vue-app/src/components/Item.vue
@@ -24,14 +24,23 @@
 
 <template>
     <div class="item-container">
-        <div class="image-container">
-            <img src="../assets/file.svg" v-if="isFile" alt="">
-            <img src="../assets/folder.svg" v-else alt="">
+        <div class="parallel-container">
+            <div class="info-container">
+                <div class="image-container">
+                    <img src="../assets/file.svg" v-if="isFile" alt="" class="logo">
+                    <img src="../assets/folder.svg" v-else alt="" class="logo">
+                </div>
+                <div class="data-container">
+                    <p class="item-name">{{ title }}</p>
+                    <p class="item-data">{{ data }}</p>
+                </div>
+            </div>
+            <div class="buttons-container">
+                <button class="button"><img src="../assets/minus.svg" class="button-image"></button>
+                <button class="button"><img src="../assets/edit.svg" class="button-image"></button>
+            </div>    
         </div>
-        <div class="data-container">
-            <p class="item-name">{{ title }}</p>
-            <p class="item-data">{{ data }}</p>
-        </div>
+        
     </div>
 </template>
 
@@ -41,6 +50,34 @@
         border-radius: 4px;
         display: flex;
         padding: 8px;
+        cursor: pointer;
+        margin-bottom: 8px;
+    }
+    .parallel-container{
+        display: flex;
+    }
+    .info-container{
+        float: left;
+    }
+    .buttons-container{
+        float: right;
+        margin-left: 4px;
+        display: flex;
+        align-items: center;
+    }
+    .button-image{
+        width: 16px;
+        height: 16px;
+    }
+    .button{
+        background-color: #FFF;
+        border: 1px solid #005;
+        border-radius: 2px;
+        cursor: pointer;
+        margin-right: 2px;
+    }
+    .button:active, .button:active img{
+        background-color: #AAA;
     }
     .image-container{
         width: fit-content;
@@ -65,4 +102,9 @@
         font-family: 'Baskerville';
         color: #005;
     }
+    .logo{
+        height: 32px;
+        width: 32px;
+    }
+
 </style>
--- vue-app/src/components/itemList.vue
+++ vue-app/src/components/itemList.vue
@@ -3,6 +3,9 @@
 
     export default {
         name: 'itemList',
+        components: {
+            Item
+        },
         data(){
             return {
                 items : [
@@ -18,46 +21,29 @@
     
 <template>
     <div v-for="item in items" class="itemContainer">
-        <div class="imageContainer">
-            <img src="../assets/file.svg" v-if="item.isFile" alt="">
-            <img src="../assets/folder.svg" v-else alt="">
-        </div>
-        <div class="dataContainer">
-            <p class="title">{{ item.title }}</p>
-            <p class="data"> {{ item.data }}</p>
-        </div>
-        
+        <Item :title="item.title" :data="item.data" :isFile="item.isFile"></Item>
+    </div>
+    <div class="agregarItem">
+        <img src="../assets/plus.svg" alt="">
+        Agregar un archivo
     </div>
 </template>
 
 <style>
-    .itemContainer{
+    .agregarItem{
         border: 1px solid #005;
-        border-radius: 8px;
+        border-radius: 4px;
         display: flex;
-        padding: 8px;
-    }
-    .imageContainer{
-        width: fit-content;
-        height: 100%;
-        float: left;
-    }
-    .dataContainer{
-        width: auto;
-        height: 100%;
-        float: right;
-        margin-left: 8px;
-    }
-    .title{
+        cursor: pointer;
         font-size: 20px;
         line-height: 20px;
         font-family: 'Baskerville';
         color:#005;
+        padding: 8px;
+        align-items: center;
     }
-    .data{
-        font-size: 12px;
-        line-height: 20px;
-        font-family: 'Baskerville';
-        color: #005;
+    .agregarItem img{
+        width: 32px;
+        height: 32px;
     }
 </style>
